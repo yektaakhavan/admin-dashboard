fdc5f01494c7588c9748672fad9e244522b0ec90
yekta_akhavan <yekta.akhavan.dev@gmail.com>
2026-08-03 20:59:45 +0330

    first commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  plugins: [pluginReact(), pluginTailwindcss()],
+});
